5dabd442461d4d825c73290dc44e176a87b3d362
Abubakar32 <mohammedabubaka32@yahoo.com>
2022-09-11 14:20:02 -0700

    4 put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+root@beeec1328d28:/alx-low_level_programming/0x00-hello_world# cat > 4-puts.c
+#!/bin/bash
+/**
+ * main -entry point
+ *
+ * Return: 0 after printing the function
+ */
+#include <stdio.h>
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+			return (0);
+}
